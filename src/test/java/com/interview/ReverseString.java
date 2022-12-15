38da54f655df662419646973843abc7615b3236c
Zafer Mengeloglu <fer.istatistik@gmail.com>
2022-12-15 21:43:35 +0300

    updating

--- src/test/java/com/interview/ReverseString.java
+++ src/test/java/com/interview/ReverseString.java
@@ -1,2 +1,31 @@
-package com.interview;public class ReverseString {
-}
+package com.interview;
+
+public class ReverseString {
+
+    public static void main(String[] args) {
+
+        String str = "amazon";
+
+        System.out.println(reverseString(str));
+
+    }
+
+    public static String reverseString(String str) {
+
+        String newString="";
+        char c;
+
+        for (int i = 0; i < str.length(); i++) {
+
+            c = str.charAt(i);
+
+            if (i!=str.length()-1)
+                newString = "-" + c + newString;
+            else
+                newString= c + newString;
+
+        }return newString;
+
+    }
+    }
+
